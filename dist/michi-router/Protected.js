e10df9a2b9e32193a95b624d56e2adf794456ee6
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-09-10 16:12:39 -0600

    feat: agregar pruebas para el componente Protected y actualizar la configuración de TypeScript

--- dist/michi-router/Protected.js
+++ dist/michi-router/Protected.js
@@ -1,29 +1,7 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importStar(require("react"));
+var jsx_runtime_1 = require("react/jsx-runtime");
+var react_1 = require("react");
 var Michi_router_1 = require("./Michi-router");
 /**
  * Un componente que restringe el acceso a sus hijos según el estado de autenticación del usuario.
@@ -61,7 +39,7 @@ function Protected(_a) {
     if (isLoading) {
         if (config.loadingComponent)
             return config.loadingComponent;
-        return config.defaultMessage ? react_1.default.createElement("div", null, "Cargando...") : null;
+        return config.defaultMessage ? (0, jsx_runtime_1.jsx)("div", { children: "Cargando..." }) : null;
     }
     return user ? children : null;
 }
