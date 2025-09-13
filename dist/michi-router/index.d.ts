47204527ad0183f7b4fc9079533acf9153647a00
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-09-13 14:01:47 -0600

    feat: eliminar archivos de definición y de implementación de componentes del enrutador

--- dist/michi-router/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { Route, RouterContextType, RouterProviderProps, LinkProps, LayoutProps, ProtectedProps } from './types';
-export { RouterProvider, Link, useNavigate } from './Michi-router';
-export { default as Protected } from './Protected';
