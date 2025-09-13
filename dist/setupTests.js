00032936473e208c9a6cdc0d3b29cde779285fdf
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-09-12 18:43:41 -0600

    chore: update package.json and tsconfig.json
    - Added a new build script "build:bundle" to package.json for bundling with tsup.
    - Included @swc/core and tsup as devDependencies in package.json.
    - Updated tsconfig.json to exclude src/setupTests.ts from compilation.

--- dist/setupTests.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Setup file for Vitest tests
-// Import jest-dom matchers so they extend Vitest's expect
-require("@testing-library/jest-dom");
